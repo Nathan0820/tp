fcfbb9b2708cd4540cd3d458908457eefe747c49
chen_haozhe <chenzmxy@gmail.com>
2026-03-13 17:46:12 +0800

    Fix CheckStyle error

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.PersonContainsKeywordsPredicate;
@@ -24,5 +25,4 @@ public class FindCommandParser implements Parser<FindCommand> {
 
         return new FindCommand(new PersonContainsKeywordsPredicate(trimmedArgs));
     }
-
 }
