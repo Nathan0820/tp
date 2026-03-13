7259091bbd3b2e8073d89e6826fc5c0085c9fd63
chen_haozhe <chenzmxy@gmail.com>
2026-03-13 18:49:25 +0800

    Fix import arrangement error

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -1,17 +1,14 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalPersons.ALICE;
-
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import static java.util.Objects.requireNonNull;
 import java.util.function.Predicate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import javafx.collections.ObservableList;
 
 import org.junit.jupiter.api.Test;
@@ -24,7 +21,9 @@ import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.order.Order;
+import static seedu.address.testutil.Assert.assertThrows;
 import seedu.address.model.person.Person;
+import static seedu.address.testutil.TypicalPersons.ALICE;
 import seedu.address.testutil.PersonBuilder;
 
 public class AddCommandTest {
