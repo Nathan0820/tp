2715df3ee4f9a75776146df3512af7e96b902c52
chen_haozhe <chenzmxy@gmail.com>
2026-03-18 19:57:41 +0800

    Fix CheckStyle error

--- src/test/java/seedu/address/logic/parser/FindOrderCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindOrderCommandParserTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.FindOrderCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.order.DeliveryTime;
 import seedu.address.model.order.Item;
 import seedu.address.model.order.Order;
@@ -22,7 +23,7 @@ public class FindOrderCommandParserTest {
     private final FindOrderCommandParser parser = new FindOrderCommandParser();
 
     @Test
-    public void parse_noArgs_returnsFindOrderCommandShowingAll() {
+    public void parse_noArgs_returnsFindOrderCommandShowingAll() throws ParseException {
         FindOrderCommand command = parser.parse("");
         Order testOrder = new Order(
                 Index.fromZeroBased(0),
@@ -60,9 +61,18 @@ public class FindOrderCommandParserTest {
     }
 
     @Test
-    public void parse_noPrefix_throwsParseException() {
-        assertParseFailure(parser, "pizza",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindOrderCommand.MESSAGE_USAGE));
+    public void parse_noPrefix_returnsFindOrderCommandShowingAll() throws ParseException {
+        // No prefix means show all orders
+        FindOrderCommand command = parser.parse("pizza");
+        Order testOrder = new Order(
+                Index.fromZeroBased(0),
+                new Item("Any Item"),
+                new Quantity("1"),
+                new DeliveryTime("2030-12-01 1800"),
+                new Address("Test Address"),
+                new Status("PREPARING")
+        );
+        assertTrue(command.getPredicate().test(testOrder));
     }
 
     @Test
